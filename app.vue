f0aa0588e1c315e1f00cd703d8ce0d609a4825f4
Kronky <66308616+Acegenesis@users.noreply.github.com>
2023-12-07 22:57:09 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,116 @@
+<template>
+    <NuxtLayout>
+      <main>
+        <div class="logo">
+          <img src="/img/logo.png" alt="Vue.js logo"/>
+        </div>
+        <button>
+          Jouer
+        </button>
+      </main>
+    </NuxtLayout>
+</template>
+
+<style>
+
+* {
+  box-sizing: border-box;
+  border: 0;
+}
+
+@font-face {
+  font-family: 'Inter';
+  src: url("/fonts/Inter.ttf") format("truetype");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,ul {
+  list-style: none;
+}
+blockquote,q {
+  quotes: none;
+}
+blockquote:before,blockquote:after,q:before,q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+html {
+  scroll-behavior: smooth;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  font-family: 'Roboto', sans-serif;
+  background-color: #f5f5f5;
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 250px;
+  background-color: #ececec;
+  border-radius: 150px;
+  box-shadow: inset 0 0 0 10px #ececec;
+  box-shadow: 0 0 10px 10px rgba(237, 237, 237, 0.2);
+}
+
+.logo img {
+  height: 100%;
+}
+
+button {
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  margin-top: 30px;
+  font-family: Inter;
+  font-size: 64px;
+  font-weight: 900;
+  line-height: 77px;
+  letter-spacing: 0.1em;
+  padding: 10px 15px;
+  color: #1AB5AC;
+  transition: all 0.2s ease-in-out;
+}
+
+button:hover {
+  transform: scale(1.05);
+  background-color: #1AB5AC;
+  color: #fff;
+  cursor: pointer;
+}
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="/img/bg.png" alt="Vue.js logo" />
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+img {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    object-position: bottom;
+}
+</style>
